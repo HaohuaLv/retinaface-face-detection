35683d216a406c5cb7f61a854f49432002044c56
HaohuaLv <81418362+HaohuaLv@users.noreply.github.com>
2023-12-24 19:45:19 +0800

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import timm
-from model import YoloModel
-from transformers import PretrainedConfig
-# model = YoloModel._from_config(PretrainedConfig.from_json_file("config.json"))
-
-# print(model.reductions)
-# # print(model.)
-print(timm.list_models("mobile*"))
